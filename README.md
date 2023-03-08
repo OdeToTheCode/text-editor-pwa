df16d16b444a1c0a5d472cb52b166493ff6a2a0e
OdeToTheCode <119718185+OdeToTheCode@users.noreply.github.com>
2023-03-08 16:12:53 -0600

    finished up the PWA

---
+++ README.md
@@ -0,0 +1,17 @@
+# Text Editor: PWA
+
+## Description
+
+This is application is an example of a PWA text editor
+
+## Installation
+
+You can download a copy from the repo.
+
+## Usage
+
+You can use this application to type localy on a machine without internet connection.
+
+## Links
+Github Account: [OdeToTheCode](https://github.com/OdeToTheCode/)
+Video: [Social API Video](https://drive.google.com/file/d/1OZHA3Sp6PJsEnB9qvjILPSCPQr1nkWNX/view?usp=sharing)
---
+++ client/src/js/database.js
@@ -0,0 +1,41 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+export const putDb = async (content) => {
+  
+  const DB_VAR = await openDB('DB_NAME', 1);
+
+  const TX_VAR = jateDb.transaction('DB_NAME', 'readwrite');
+
+  const STORE_VAR = tx.objectStore('DB_NAME');
+
+  const VAR_NAME = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
+};
+
+export const getDb = async () => {
+  const DB_VAR = await openDB('DB_NAME', 1);
+  const TX_VAR = jateDb.transaction('DB_NAME', 'readonly');
+  const STORE_VAR = tx.objectStore('DB_NAME');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  return result?.value;
+};
+
+initdb();
